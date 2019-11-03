086ee53efff12a54b7268c96c4ecbfa485dba7f1
benwei1314 <55177022+benwei1314@users.noreply.github.com>
2019-11-02 23:33:49 -0700

    Update leds.ino

--- leds.ino
+++ leds.ino
@@ -1,30 +1,72 @@
-void setup() {
-  // put your setup code here, to run once:
-pinMode(6,OUTPUT);
-pinMode(8,OUTPUT);
-pinMode(10,OUTPUT);
-pinMode(12,OUTPUT);
+void setup()
+{
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(2, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-digitalWrite(6,HIGH);
-delay(100);
-digitalWrite(6,LOW);
-delay(100);
-
-digitalWrite(8,HIGH);
-delay(100);
-digitalWrite(8,LOW);
-delay(100);
-
-digitalWrite(10,HIGH);
-delay(100);
-digitalWrite(10,LOW);
-delay(100);
-
-digitalWrite(12,HIGH);
-delay(100);
-digitalWrite(12,LOW);
-delay(100);
+void loop()
+{
+  digitalWrite(10, HIGH);
+  digitalWrite(9, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(2, LOW);
+  delay(250); // Wait for 250 millisecond(s)
+  digitalWrite(10, LOW);
+  digitalWrite(9, HIGH);
+  digitalWrite(8, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(2, LOW);
+  delay(250); // Wait for 250 millisecond(s)
+  digitalWrite(10, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(8, HIGH);
+  digitalWrite(3, LOW);
+  digitalWrite(2, LOW);
+  delay(250); // Wait for 250 millisecond(s)
+  digitalWrite(10, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(3, HIGH);
+  digitalWrite(2, LOW);
+  delay(250); // Wait for 250 millisecond(s)
+  digitalWrite(10, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(2, HIGH);
+  delay(250); // Wait for 250 millisecond(s)
+  digitalWrite(10, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(2, HIGH);
+  delay(250); // Wait for 250 millisecond(s)
+  digitalWrite(10, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(3, HIGH);
+  digitalWrite(2, LOW);
+  delay(250); // Wait for 250 millisecond(s)
+  digitalWrite(10, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(8, HIGH);
+  digitalWrite(3, LOW);
+  digitalWrite(2, LOW);
+  delay(250); // Wait for 250 millisecond(s)
+  digitalWrite(10, LOW);
+  digitalWrite(9, HIGH);
+  digitalWrite(8, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(2, LOW);
+  delay(250); // Wait for 250 millisecond(s)
+  digitalWrite(10, HIGH);
+  digitalWrite(9, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(2, LOW);
+  delay(250); // Wait for 250 millisecond(s)
 }
